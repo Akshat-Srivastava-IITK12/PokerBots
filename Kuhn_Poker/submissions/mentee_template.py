80b8e1c4aa940aedee335b82d8af1eb6d4af0cc6
Srisharan Reddy Allala <74013655+Srisharan268@users.noreply.github.com>
2026-06-08 23:38:43 +0530

    Bot code
    Updated the MenteeKuhnBot class to perkeo and modified action logic.

--- Kuhn_Poker/submissions/mentee_template.py
+++ Kuhn_Poker/submissions/mentee_template.py
@@ -1,6 +1,6 @@
 import random
 
-class MenteeKuhnBot:
+class perkeo:
     """
     INSTRUCTIONS:
     1. Rename this class to your Team Name (e.g., TeamAlphaBot).
@@ -8,14 +8,42 @@ class MenteeKuhnBot:
     3. Return exactly 'p' (Pass/Fold) or 'b' (Bet/Call).
     """
     def __init__(self):
-        self.name = "Mentee_Template" # Change this to your team name
+        self.name = "perkeo" # Change this to your team name
 
     def get_action(self, state) -> str:
         # Access state variables:
         # state.my_card ('J', 'Q', or 'K')
         # state.history (e.g., '', 'p', 'b', 'pb')
+        bluff_king = 0.2
+        bluff_jack = 0.4
+        bluff_queen = 0.4
+
+        rand = random.random()
         
         # Example heuristic logic:
-        if state.my_card == 'K': return 'b'
-        if state.my_card == 'J': return 'p'
+        if state.my_card == 'K':
+            if rand < bluff_king and state.history == '':
+                return 'p'
+            else:
+                return 'b'
+            
+        elif state.my_card == 'J': 
+            if state.history == 'b' or state.history == 'pb':
+                return 'p'
+            else:
+                if rand< bluff_jack
+                    return 'b'    #bluffing when the opponent did not bet
+                else:
+                    return 'p'
+
+        elif state.my_card == 'Q':
+            if state.history == 'p' and rand < 2*bluff_queen:
+                return 'b'
+            elif state.history == '' and rand < bluff_queen:
+                return 'b'
+            elif (state.history == 'pb' or state.history == 'b') and rand < bluff_queen/2:
+                return 'b'
+            else:
+                return 'p'
+                
         return random.choice(['p', 'b'])
